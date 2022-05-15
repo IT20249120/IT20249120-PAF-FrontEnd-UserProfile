b792cec661efbee7efd81e5a2e21aa95b0cdf32b
IT20249120 <89574403+IT20249120@users.noreply.github.com>
2022-05-16 00:55:08 +0530

    electro DB

---
+++ electro.sql
@@ -0,0 +1,9 @@
+/*
+-- Query: SELECT * FROM electro.user
+LIMIT 0, 1000
+
+-- Date: 2022-05-16 00:41
+*/
+INSERT INTO `` (`UID`,`name`,`password`,`phoneNO`,`email`,`address`,`accountNO`,`createdDate`) VALUES (1,'piya',12345,1234567898,'piya@gmail.com','malabe',12,'2020-02-06');
+INSERT INTO `` (`UID`,`name`,`password`,`phoneNO`,`email`,`address`,`accountNO`,`createdDate`) VALUES (2,'thi',67890,987656456,'thi@gmail.com','colombo',345,'2020-04-23');
+INSERT INTO `` (`UID`,`name`,`password`,`phoneNO`,`email`,`address`,`accountNO`,`createdDate`) VALUES (3,'mashi',54673,734567876,'mashi@gmail.com','kaluthra',923,'2020-08-04');
